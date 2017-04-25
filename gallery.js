dea6d052a4f62875741da80dc734269d0cb5c80b
Unknown <mshimada2@mail.csuchico.edu>
2017-04-25 13:07:02 -0700

    added console logs; made a do while; edited it so it could push out a random value each time and it couldnt repeat itself

--- gallery.js
+++ gallery.js
@@ -1,6 +1,17 @@
+var chico = ["Bidwell Park", "Alpha Gamme Rho Fraternity House", "Chico State", "The Flumes", "Chico Mall"];
 
+var current;
 
-function shuffleGallery()
-{
+function shuffleGallery(){
 
+//console.log(chico[Math.floor(Math.random() * chico.length)]);
+
+do{
+var store = Math.floor(Math.random() * chico.length);
+
+}
+while(chico[store] == current);
+
+current = chico[store];
+console.log(chico[store]);
 }

938881138143157a7f13bf27bf79d6aeb9f2df44
Unknown <mshimada2@mail.csuchico.edu>
2017-04-27 13:14:13 -0700

    made it so that theres pictures and instead of just the places names

--- gallery.js
+++ gallery.js
@@ -1,4 +1,20 @@
-var chico = ["Bidwell Park", "Alpha Gamme Rho Fraternity House", "Chico State", "The Flumes", "Chico Mall"];
+ var chico = [
+   {description: "Bidwell Park",
+    picture: "http://bidwellpark.org/wp-content/uploads/2014/04/BidwellPool.jpg",
+    from: "bidwellpark.org"},
+   {description: "Alpha Gamme Rho Fraternity House",
+   picture: "http://cdn.totalfratmove.com/wp-content/uploads/2016/02/1233ecf67466f88fb89153c5359af6df.jpg",
+   from: "totalfratmove.com"},
+   {description: "Chico State",
+   picture: "https://i.ytimg.com/vi/bpZIGBJVErY/maxresdefault.jpg",
+   from:"YouTube"},
+   {description: "Bear Hole",
+   picture:"https://media-cdn.tripadvisor.com/media/photo-s/02/57/ad/31/bear-hole.jpg",
+   from:"www.tripadvisor.com"},
+   {description:"Monkey Face",
+   picture:"https://s3.amazonaws.com/storage.filemobile.com/storage/25463209/15",
+   from: "ulocal.kcra.com"}
+ ];
 
 var current;
 
@@ -13,5 +29,7 @@ var store = Math.floor(Math.random() * chico.length);
 while(chico[store] == current);
 
 current = chico[store];
-console.log(chico[store]);
+document.getElementById("picture").src = current.picture;
+document.getElementById("caption").innerHTML = current.description;
+document.getElementById("source").innerHTML = "Image courtesy of " + current.from;
 }
